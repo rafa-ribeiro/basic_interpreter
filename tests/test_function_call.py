e5721140d38515310eb4831ae107025ee556248b
Rafael Ribeiro <tecinfo.rafael@gmail.com>
2024-09-24 22:45:44 -0300

    fix: Add some tests and fix error return on execute func call

---
+++ tests/test_function_call.py
@@ -0,0 +1,22 @@
+from basic_interpreter import basic
+
+
+def test__function_call_works():
+    command = "FUN soma(a, b) -> a + b"
+    _, _ = basic.run("<stdin>", command)
+    result, error = basic.run("<stdin>", "soma(3, 5)")
+    assert result.value == 8
+
+
+def test__function_call_with_too_few_args():
+    command = "FUN soma(a, b) -> a + b"
+    _, _ = basic.run("<stdin>", command)
+    result, error = basic.run("<stdin>", "soma(2)")
+    assert error.details == "1 too few args passed into 'soma'"
+
+
+def test__function_call_with_too_many_args():
+    command = "FUN soma(a, b) -> a + b"
+    _, _ = basic.run("<stdin>", command)
+    result, error = basic.run("<stdin>", "soma(2, 3, 3)")
+    assert error.details == "1 too many args passed into 'soma'"
